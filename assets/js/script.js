39c95cf32e981455efd15d6dd35eb7acc5399416
David Marsh <40401344+Daveym1@users.noreply.github.com>
2022-12-09 20:27:13 +0000

    added some style to the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,22 @@
 $(document).ready(function(){
 
-    var currentDay = $('#currentDay');
+ $(".saveBtn").on("click", function(){
+
+var value = $(this).siblings(".list-content").val();
+var time = $(this).siblings(".hour")
+
+ });
+
+    
+  
+  var currentDay = $('#currentDay');
     currentDay.text = moment();
 
     // var currentDay = document.getElementById("currentDay");
     // currentDay.textContent = moment();
 
+   
+ 
 
   
   });
